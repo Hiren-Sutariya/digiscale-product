0c7a77aaf7c0d0570b3db9f8c1944ea74fa7fb8c
Sutariya-Hiren <sutariyahiren14@gmail.com>
2026-07-23 22:04:56 +0530

    fix: update CORS middleware to allow all origins via regex

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,9 +39,7 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-    ],
+    allow_origin_regex="https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
